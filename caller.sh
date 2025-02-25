b7d58dca9593ef9bfed5c58185d5589b99adb3e8
Pasquale Lodise <pakilodi@gmail.com>
2025-02-25 08:43:51 +0100

    Added colors in case of success and error

--- caller.sh
+++ caller.sh
@@ -6,10 +6,19 @@ total_requests=0
 status_codes=()
 
 function finish {
+   # Colour codes
+    RED='\033[0;31m'
+    GREEN='\033[0;32m'
+    NC='\033[0m'
+
     echo "Script terminated. Calculating status code percentages..."
     for code in "${!status_codes[@]}"; do
-        percent=$(echo "scale=2; ${status_codes[$code]} * 100 / $total_requests" | bc)
-        echo "Status code $code: $percent%"
+        percentage=$(echo "scale=2; ${status_codes[$code]} / $total_requests * 100" | bc)
+        if [ $code -eq 200 ]; then
+            echo -e "Status code: ${GREEN}$code${NC} - ${status_codes[$code]} requests - $percentage%"
+        else
+            echo -e "Status code: ${RED}$code${NC} - ${status_codes[$code]} requests - $percentage%"
+        fi
     done
     exit 0
 }
